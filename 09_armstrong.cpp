e8bf4c846a71ff6774190fc940fa8634f4b01382
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-03-10 10:37:33 +0500

    updated

--- 09_armstrong.cpp
+++ 09_armstrong.cpp
@@ -1,34 +1,40 @@
 #include<iostream>
 using namespace std;
 
-
-
-int main(){
-   int num;
+ bool checkArmstrong(int num){
+  
    int digit=0;
    int result=0;
-   cout<<"Enter Number : ";
-   cin>>num;
+  
 
    int mainnum=num;
 
    while(num != 0){
-    digit=num%10;
-    cout<<"digit = "<<digit<<endl;
-    
-    digit=digit * digit * digit;
-   
-   cout<<digit<<"After loop digit"<<endl;
-   result += digit;
+    digit=num%10; //extract last digit 
 
-   num=num/10;
+    result += digit * digit * digit; //find its armstrong
+
+    num=num/10;      //reduce the last digit
    }
-   cout<<"Result : "<<result<<endl;
-   if(mainnum == result){
-    cout<<mainnum<<" is an Arm strong Number"<<endl;
+    if(mainnum == result){
+   return true;
    }
    else{
-    cout<<mainnum<<" is not an Arm strong number.";
+    return false;
+   }
+ }
+
+int main(){
+   int num;
+   cout<<"Enter Number : ";
+   cin>>num;
+   bool result = checkArmstrong(num);
+
+   if(result){
+    cout<<"it is an Arm strong Number"<<endl;
+   }
+   else{
+    cout<<"it is not an Arm strong number.";
    }
    
 }
