f3075d0fa0a11748a72814289d059af73bc0b2b8
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-04-05 23:04:23 +0500

    Sliding Window

---
+++ sliding_window_max_sum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 3, 4, 7, 8, 2, 1, 9, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int k = 3;   // Window size
+
+    // Edge case check
+    if (k > size) {
+        cout << "Invalid window size!";
+        return 0;
+    }
+
+    //  Calculate first window sum
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+        sum += arr[i];
+    }
+
+    int largest = sum;
+
+    //  Slide the window
+    for (int i = k; i < size; i++) {
+        sum = sum + arr[i] - arr[i - k];
+
+        if (sum > largest) {
+            largest = sum;
+        }
+    }
+
+    cout << "\nLargest Sum = " << largest << endl;
+
+    return 0;
+}
